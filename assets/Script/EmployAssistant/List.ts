219ef47d8a5d87fd04d26079b44576b6863aeaae
zjgwcg <514804899@qq.com>
2018-12-22 21:10:08 +0800

    gengxin

--- assets/Script/EmployAssistant/List.ts
+++ assets/Script/EmployAssistant/List.ts
@@ -6,10 +6,17 @@ const {ccclass, property} = cc._decorator;
 @ccclass
 export default class List extends cc.Component {
 
+    min_beginning_assistant_property = 1;
+    max_beginning_assistant_property = 20;
+    beginning_assistant_lvl = 0;
+    beginning_assistant_exp = 0;
+    beginning_assistant_skill_amount = 3;
+
+
     @property(cc.Prefab)
     item:cc.Prefab = null;
 
-    assistantResumeData ={
+    assistandData ={
         assistant_id: 0 ,
         assistant_name: "默认",
         assistant_icon:"assistant001",
@@ -22,21 +29,12 @@ export default class List extends cc.Component {
         assistant_skill:[],
     };
 
+
+
     namepool:string[] = [];
     skillpool:number[] = [];
-    skillnamepool:string[] = [];
-    iconpool:string[] = [];
-    
-//"assistant001","assistant002","assistant003","assistant004","assistant005","assistant006"
-
-    min_beginning_assistant_property = 1;
-    max_beginning_assistant_property = 20;
-    beginning_assistant_lvl = 0;
-    beginning_assistant_exp = 0;
-    beginning_assistant_skill_amount = 3;
-
-
-    
+    //skillnamepool:string[] = [];
+    iconpool:string[] = [];     
     items:cc.Node[] = [];
     // LIFE-CYCLE CALLBACKS:
 
@@ -45,49 +43,39 @@ export default class List extends cc.Component {
         this.loadsetting();
         //this.loadAssistant();
 
-        /*for(let i in this.namepool){
-            console.log(i);
-        }
-        for(let i in this.skillnamepool){
-            console.log(i);
-        }
-        for(let i in this.skillpool){
-            console.log(i);
-        }   */
-
         this.initListData();
     }
 
     loadsetting(){
         //读取json配置文件
-        
-        
-
-        this.min_beginning_assistant_property = JsonConfig.getItem(ConfigType.Setting,1).setting_value;
-        this.max_beginning_assistant_property = JsonConfig.getItem(ConfigType.Setting,2).setting_value;
-        this.beginning_assistant_lvl = JsonConfig.getItem(ConfigType.Setting,3).setting_value;
-        this.beginning_assistant_exp = JsonConfig.getItem(ConfigType.Setting,4).setting_value;
-        this.beginning_assistant_skill_amount = JsonConfig.getItem(ConfigType.Setting,5).setting_value;
-
-        for(let i in JsonConfig.getItem(ConfigType.Randompool,3)){
             
-            if(i=="id"||i=="random_name"){
-                
-            }
-            else
-            {
-                this.namepool.push(JsonConfig.getItem(ConfigType.Randompool,3)[i]);
+
+        //this.min_beginning_assistant_property = JsonConfig.getItem(ConfigType.Setting,1).setting_value;
+        //this.max_beginning_assistant_property = JsonConfig.getItem(ConfigType.Setting,2).setting_value;
+        //this.beginning_assistant_lvl = JsonConfig.getItem(ConfigType.Setting,3).setting_value;
+        //this.beginning_assistant_exp = JsonConfig.getItem(ConfigType.Setting,4).setting_value;
+        //this.beginning_assistant_skill_amount = JsonConfig.getItem(ConfigType.Setting,5).setting_value;
+
+        //读取随机姓名池
+        for(let i in JsonConfig.getAllItem(ConfigType.Randompool)){
+            
+            if(JsonConfig.getAllItem(ConfigType.Randompool)[i].Randompool!="NA"){//尾部NA舍去
+                console.log(i);
+                console.log(JsonConfig.getAllItem(ConfigType.Randompool)[i].random_name);
+                this.namepool.push(JsonConfig.getAllItem(ConfigType.Randompool)[i].random_name);
             }
         }
-        for(let i in JsonConfig.getItem(ConfigType.Randompool,2)){
-            if(i=="id"||i=="random_name"){
-                
-            }
-            else
-            {
-                this.iconpool.push(JsonConfig.getItem(ConfigType.Randompool,2)[i]);
+
+
+        for(let i in JsonConfig.getAllItem(ConfigType.Randompool)){
+            
+            if(JsonConfig.getAllItem(ConfigType.Randompool)[i].random_icon!="NA"){//尾部NA舍去
+                console.log(i);
+                console.log(JsonConfig.getAllItem(ConfigType.Randompool)[i].random_icon);
+                this.iconpool.push(JsonConfig.getAllItem(ConfigType.Randompool)[i].random_icon);
             }
         }
+
         
         
         for(let i in JsonConfig.getAllItem(ConfigType.Skills)){
@@ -95,8 +83,8 @@ export default class List extends cc.Component {
             for(let j in JsonConfig.getAllItem(ConfigType.Skills)[i]){
                 if(j=="id")
                 this.skillpool.push(JsonConfig.getAllItem(ConfigType.Skills)[i][j]);
-                if(j=="skill_name")
-                this.skillnamepool.push(JsonConfig.getAllItem(ConfigType.Skills)[i][j]);
+                //if(j=="skill_name")
+                //this.skillnamepool.push(JsonConfig.getAllItem(ConfigType.Skills)[i][j]);
             }
         }
 
@@ -121,39 +109,46 @@ export default class List extends cc.Component {
         return min+Math.floor(Math.random()*(max-min+1));
     }
 
-    saveAssistant(assistantResumeData){
+    saveAssistant(assistantData){
         
     }
 
 
-    loadAssistant(){
-        let list:any = [[]];
+    loadAssistantlist(){
+        let list:any = [];
         list = JsonConfig.getAllItem(ConfigType.Assisitant);
         for(let i in list){
-            console.log(i);
-        }
-        
-       
+            JsonConfig.getAllItem(ConfigType.Assisitant)[i];
+        }   
+        return list;
     }
+
+    loadAssistant(id:number){//返回键值对
+        let list = this.loadAssistantlist();
+        return list[id];
+    }
+
+
+
 
 
     newassistant(){
-        this.assistantResumeData.assistant_id = 1;
-        this.assistantResumeData.assistant_name = this.namepool[this.random(0,this.namepool.length-1)];
-        this.assistantResumeData.assistant_icon = this.iconpool[this.random(0,this.iconpool.length-1)];
-        this.assistantResumeData.assistant_lvl = this.beginning_assistant_lvl;
-        this.assistantResumeData.assistant_exp = this.beginning_assistant_exp;
-        this.assistantResumeData.assistant_eloquence = this.random(this.min_beginning_assistant_property,this.max_beginning_assistant_property);
-        this.assistantResumeData.assistant_intelligence = this.random(this.min_beginning_assistant_property,this.max_beginning_assistant_property);
-        this.assistantResumeData.assistant_knowledge = this.random(this.min_beginning_assistant_property,this.max_beginning_assistant_property);
-        this.assistantResumeData.assistant_operation = this.random(this.min_beginning_assistant_property,this.max_beginning_assistant_property);
-        this.assistantResumeData.assistant_skill = [];
+        this.assistandData.assistant_id = 1;
+        this.assistandData.assistant_name = this.namepool[this.random(0,this.namepool.length-1)];
+        this.assistandData.assistant_icon = this.iconpool[this.random(0,this.iconpool.length-1)];
+        this.assistandData.assistant_lvl = this.beginning_assistant_lvl;
+        this.assistandData.assistant_exp = this.beginning_assistant_exp;
+        this.assistandData.assistant_eloquence = this.random(this.min_beginning_assistant_property,this.max_beginning_assistant_property);
+        this.assistandData.assistant_intelligence = this.random(this.min_beginning_assistant_property,this.max_beginning_assistant_property);
+        this.assistandData.assistant_knowledge = this.random(this.min_beginning_assistant_property,this.max_beginning_assistant_property);
+        this.assistandData.assistant_operation = this.random(this.min_beginning_assistant_property,this.max_beginning_assistant_property);
+        this.assistandData.assistant_skill = [];
             for(let i= 0; i < this.beginning_assistant_skill_amount;i++){
-                this.assistantResumeData.assistant_skill.push(this.skillnamepool[this.random(0,this.skillnamepool.length-1)]); 
+                this.assistandData.assistant_skill.push(this.skillpool[this.random(0,this.skillpool.length-1)]); 
             }
         
 
-        return this.assistantResumeData;
+        return this.assistandData;
     }
 
     public refresh(){

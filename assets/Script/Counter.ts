dd9780052b07f30862a36df0cf5338b07cbb2a3d
lynch521 <zjuwlq521@gmail.com>
2019-01-14 23:52:07 +0800

    柜台类

--- assets/Script/Counter.ts
+++ assets/Script/Counter.ts
@@ -1,4 +1,5 @@
 import { Assistant } from "./Assistant";
+import { ConfigType, JsonConfig } from "./JsonConfig";
 
 /**柜台类
  * 柜台决定提供的服务和售卖的商品
@@ -9,15 +10,48 @@ export class Counter {
     /** 柜台模板ID */
     tempID:number;
     /** 业务数据
-     * 对商品来说以{业务id：库存量}形式存储
+     * 对商品来说以{业务id:{storage:10,补货上限max：100}}形式存储
      * 对服务来说{业务id：每周可使用次数}形式存储
      */
     serviceData:any = {};
     /**柜台上的职工 */
     assistant:Assistant;
 
-    constructor()
+    constructor(json:any = null,type:number)
     {
-    
+        if(json)
+        {
+
+        }
+        else
+        {
+            switch(type)
+            {
+                case 0:
+                    break;
+                case 1:
+                    let counterConfig:any = JsonConfig.getItem(ConfigType.Assisitant,this.tempID);
+                    let serviceList:string[] = (counterConfig.serviceList as string).split("|");
+                    for(let i=0;i<serviceList.length;i++)
+                    {
+                        let key = Number(serviceList[i]);
+                        this.serviceData = {key:{storage:0,max:0}};
+                    }
+                    break;
+            }  
+        }
+    }
+
+    /**商品自动补货*/
+    autoReplenishment():void {
+        
+        let counterConfig:any = JsonConfig.getItem(ConfigType.Assisitant,this.tempID);
+        let storage:number = counterConfig.storage;
+
+        for(let i in this.serviceData)
+        {           
+            let serviceConfig:any = JsonConfig.getItem(ConfigType.Assisitant,Number(i));
+        }
+        
     }
 }
--- assets/Script/CounterManager.ts
+++ assets/Script/CounterManager.ts
@@ -1,3 +1,5 @@
+import { Counter } from "./Counter";
+
 // Learn TypeScript:
 //  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/typescript.html
 //  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/typescript.html
@@ -10,5 +12,26 @@
 
 export class CounterManager {
 
+    /**单例 */
+    private static ins : CounterManager;
 
+    /**柜台列表 */
+    _CounterList:Counter[] = [];
+
+    private constructor()
+    {
+       
+    }
+
+    /**
+     * 获取CounterManager的单例
+    */
+   static getIns():CounterManager
+   {
+       if (CounterManager.ins == null) {
+        CounterManager.ins = new CounterManager();
+       }
+
+       return CounterManager.ins;
+   }
 }
